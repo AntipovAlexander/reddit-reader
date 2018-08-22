fa1ff75558c1c105b3b544adf1cc376107452eea
Alexander Antipov <antipovaleksandr95@gmail.com>
2018-08-22 13:01:36 +0300

    added test

--- app/src/main/java/com/antipov/redditreader/data/pojo/Top.java
+++ app/src/main/java/com/antipov/redditreader/data/pojo/Top.java
@@ -2,12 +2,28 @@ package com.antipov.redditreader.data.pojo;
 
 import com.google.gson.annotations.SerializedName;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Top {
     @SerializedName("kind")
     private String kind;
 
     @SerializedName("data")
     private Data data;
+
+    /**
+     * @return model for unit test
+     */
+    public static Top getForTest() {
+        Top t = new Top();
+        Data d = new Data();
+        List<Child> children = new ArrayList<>();
+        children.add(new Child());
+        d.setChildren(children);
+        t.setData(d);
+        return t;
+    }
 
     public String getKind() {
         return kind;
---
+++ app/src/test/java/com/antipov/redditreader/ui/activity/main/MainPresenterImplTest.java
@@ -0,0 +1,75 @@
+package com.antipov.redditreader.ui.activity.main;
+
+import com.antipov.redditreader.data.pojo.Top;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentMatchers;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.Arrays;
+
+import rx.Observable;
+
+@RunWith(MockitoJUnitRunner.class)
+public class MainPresenterImplTest {
+
+    @Mock
+    MainView mockedView;
+
+    @Mock
+    MainInteractor mockedInteractor;
+
+    MainPresenter<MainView, MainInteractor> presenter;
+
+    @Before
+    public void setUp(){
+        presenter = new MainPresenterImpl<>(mockedInteractor);
+        presenter.attachView(mockedView);
+    }
+
+    /**
+     * method to test positive scenario with mocked interactor
+     */
+    @Test
+    public void loadTopPostsPositive() {
+        int limit = 10;
+
+        Mockito.doReturn(Observable.just(Top.getForTest()))
+                .when(mockedInteractor)
+                .loadTopPosts(limit);
+        presenter.loadTopPosts(limit);
+        Mockito.verify(mockedView).showLoadingFullscreen();
+        Mockito.verify(mockedView).renderList(ArgumentMatchers.anyList());
+        Mockito.verify(mockedView).hideLoadingFullscreen();
+        Mockito.verifyNoMoreInteractions(mockedView);
+    }
+
+    /**
+     * method to test negative scenario with mocked interactor
+     */
+    @Test
+    public void loadTopPostsNegative() {
+        int limit = 10;
+
+        Mockito.doReturn(Observable.just(new Throwable()))
+                .when(mockedInteractor)
+                .loadTopPosts(limit);
+        presenter.loadTopPosts(limit);
+        Mockito.verify(mockedView).showLoadingFullscreen();
+        Mockito.verify(mockedView).showError(ArgumentMatchers.anyString());
+        Mockito.verify(mockedView).hideLoadingFullscreen();
+        Mockito.verifyNoMoreInteractions(mockedView);
+    }
+
+    @Test
+    public void onRecyclerClicked() {
+        String url = "https://www.google.com/";
+        presenter.onRecyclerClicked(url);
+        Mockito.verify(mockedView).startChromeTab(url);
+        Mockito.verifyNoMoreInteractions(mockedView);
+    }
+}
